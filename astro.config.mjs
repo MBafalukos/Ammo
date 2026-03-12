c5a58619d1cf7c96646ff16405213aa558952bb2
MBafalukos <168361687+MBafalukos@users.noreply.github.com>
2026-03-12 16:11:08 +0200

    Astro Config for GH Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
-  
+  site: 'https://github.com/MBafalukos/Ammo.git',
+  base: '/Ammo', // Replace with your actual repository name
   vite: {
     plugins: [tailwindcss()],
   },
